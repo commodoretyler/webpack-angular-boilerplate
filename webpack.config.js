799f02d73093f4b218e089ad836b9aaaa516c502
Tyler Moore <tylmoore@expedia.com>
2017-07-02 15:19:48 -0500

    update webpack config with v2 api

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
+var path = require('path');
+
 module.exports = {
   entry: "./src/app.js",
   output: {
-    path: __dirname,
+    path: path.resolve(__dirname, 'dist'),
     filename: "bundle.js"
   },
   devtool: 'inline-source-map',
   module: {
-    loaders: [
-      { test: /\.css$/, loader: "style!css" },
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
       { test: /\.scss$/, loaders: ['style', 'css', 'sass'] },
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
     ]
   }
 };
